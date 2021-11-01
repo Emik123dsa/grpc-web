7f87880d910cba884139bbbda55b05aebc27009a
Emik123dsa <pengrobinson@mail.ru>
2021-11-01 08:32:23 +0300

    feat: added tsconfig.schema

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,7 @@
 import * as http from 'http'
 
+import { networkInterfaces } from 'os'
+
 const handleRequest: (
   req: http.IncomingMessage,
   res: http.ServerResponse,
@@ -8,8 +10,23 @@ const handleRequest: (
   res: http.ServerResponse,
   next?: () => void,
 ): void => {
+  const nets = networkInterfaces()
+  const results = Object.create(null)
+
+  for (const name of Object.keys(nets)) {
+    for (const net of nets[name]) {
+      // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
+      if (net.family === 'IPv4' && !net.internal) {
+        if (!results[name]) {
+          results[name] = []
+        }
+        results[name].push(net.address)
+      }
+    }
+  }
+
   res.writeHead(200)
-  return res.end(null)
+  return res.end(JSON.stringify(results.eth0))
 }
 
 const server = http.createServer(handleRequest)
